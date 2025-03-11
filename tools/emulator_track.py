e9340b09c5a74fc741c94ace3ab24c0a4e7ed9c6
Mitko Haralanov <voidtrance@gmail.com>
2025-03-11 09:47:51 -0700

    Add live print tracking to emulator_track.py
    The tool draw_toolhead.py is renamed to emulator_track.py. In
    addition live tracking is added.
    
    Live tracking monitors the toolhead and heater temperatures and
    draw graphs in real time.

--- tools/emulator_track.py
+++ tools/emulator_track.py
@@ -20,7 +20,10 @@ import time
 import socket
 import pickle
 import argparse
+import threading
 import plotly.graph_objects as go
+from dash import Dash, dcc, html, set_props
+from dash.dependencies import Input, Output
 from vortex.controllers.types import ModuleTypes
 import collections
 from weakref import proxy
@@ -106,26 +109,36 @@ def connect(path):
         return None
     return sock
 
-def get_toolhead_id(conn):
-    request = {"request" : "object_list"}
+def get(conn, request):
     conn.sendall(pickle.dumps(request))
     data = conn.recv(8192)
-    response = pickle.loads(data)
+    return pickle.loads(data)
+
+def get_toolhead_id(conn):
+    response = get(conn, {"request" : "object_list"})
     toolheads = response["objects"][ModuleTypes.TOOLHEAD]
     if len(toolheads) > 1:
         return 0
     return toolheads[0]["id"]
 
-def get_position(connection, id):
-    request = {"request": "query", "objects": [id]}
-    connection.sendall(pickle.dumps(request))
-    data = connection.recv(8192)
-    response = pickle.loads(data)
-    idxs = [i for i in response[id]["axes"] if i != 7]
-    pos = [response[id]["position"][i] for i in idxs]
-    return pos
+def get_heaters_id(conn):
+    response = get(conn, {"request" : "object_list"})
+    heaters = response["objects"][ModuleTypes.HEATER]
+    return [x["id"] for x in heaters]
 
-def draw(filename, data):
+def get_objects(conn, *args):
+    return get(conn, {"request": "query", "objects": list(args)})
+
+def get_data(conn, toolhead, *heaters):
+    state = get_objects(conn, toolhead, *heaters)
+    idxs = [i for i in state[toolhead]["axes"] if i != 7]
+    pos = [state[toolhead]["position"][i] for i in idxs]
+    temps = dict.fromkeys(heaters, 0.)
+    for h in heaters:
+        temps[h] = state[h]["temperature"]
+    return pos, temps
+
+def draw(filename, data, times, heaters):
     x = [x[0] for x in data]
     y = [x[1] for x in data]
     z = [x[2] for x in data]
@@ -133,6 +146,12 @@ def draw(filename, data):
     l = go.Layout(autosize=False, width=1800, height=1800,
                   margin=go.layout.Margin(l=50, r=50, b=100, t=100, pad=4))
     fig = go.Figure(g, layout=l)
+
+    tfig = go.Figure()
+    for h in heaters:
+        t = go.Scatter(x=times, y=heaters[h], mode="lines", line_shape="spline", name=f"heater {h}")
+        tfig.add_trace(t)
+
     prefix = """<html>
 <head>
 <style>
@@ -142,15 +161,81 @@ th { text-align: left; background-color: #e4f0f5; }
 th.title { text-align: center; background-color: #3f87a6; }
 </style>
 </head>
-<body>"""
-    suffix = "</body></html>"""
+<body>
+<table>"""
+    suffix = "</table></body></html>"""
 
     filename.write(prefix)
+    filename.write("<tr><th class='title'>Toolhead trace</th></tr><tr><td>")
     html = fig.to_html(full_html=False, include_plotlyjs=True)
     html = html.replace("<div>", "<div class=\"plotly-graph-top\">")
     filename.write(html)
-    filename.write("</td></tr></table></div></div>")
+    filename.write("</td></tr>")
+    filename.write("<tr><th class='title'>Heater Temps</th></tr><tr><td>")
+    html = tfig.to_html(full_html=False, include_plotlyjs=False)
+    html = html.replace("<div>", "<div class=\"plotly-graph-top\">")
+    filename.write(html)
+    filename.write("</td></tr>")
     filename.write(suffix)
+
+def run_dash_server(connection, toolhead, *heaters):
+    def stop_app(err):
+        set_props('interval-component', dict(disabled=True))
+        set_props('temp-interval-component', dict(disabled=True))
+
+    app = Dash("Emulator Trace", on_error=stop_app)
+
+    g = go.Scatter3d(name="shape", mode="lines", x=[], y=[], z=[])
+    l = go.Layout(autosize=False, width=1800, height=1800,
+                    margin=go.layout.Margin(l=50, r=50, b=100, t=100, pad=4))
+    fig = go.Figure(g, layout=l)
+    
+    tfig = go.Figure()
+    for h in heaters:
+        t = go.Scatter(x=[], y=[], mode="lines", line_shape="spline", name=f"heater {h}")
+        tfig.add_trace(t)
+
+    app.layout = html.Div([
+        html.Div([
+            html.Button('Stop Refresh', id='refresh-control', n_clicks=0)
+        ], title="Refresh Control"),
+        html.Div([
+            dcc.Graph(id="travel-plot", figure=fig),
+            dcc.Interval(id="interval-component", interval=40, n_intervals=0)
+        ]),
+        html.Div([
+            dcc.Graph(id="heater-temps", figure=tfig),
+            dcc.Interval(id="temp-interval-component", interval=1000, n_intervals=0)
+        ])
+    ])
+
+    start_time = time.time()
+
+    @app.callback(Output('travel-plot', 'extendData'),
+                  Input('interval-component', 'n_intervals'))
+    def update_path_data(n):
+        pos, temps = get_data(connection, toolhead, *heaters)
+        print(f"X={pos[0]}, Y={pos[1]}, Z={pos[2]}")
+        data = {'x':[[pos[0]]], 'y': [[pos[1]]], 'z': [[pos[2]]]}
+        return data
+
+    @app.callback(Output('heater-temps', 'extendData'),
+                  Input('interval-component', 'n_intervals'))
+    def update_temp_data(n):
+        pos, temps = get_data(connection, toolhead, *heaters)
+        runtime = round(time.time() - start_time, 3)
+        runtime = time.strftime("%H:%M:%S", time.localtime(runtime))
+        data = {'x': [[runtime], [runtime]], 'y':[[temps[h]] for h in heaters]}
+        return data, [0, 1], 100
+    
+    @app.callback(Output('interval-component', 'disabled'),
+                  Output('refresh-control', 'children'),
+                  Input('refresh-control', 'n_clicks'))
+    def refresh_control(n_clicks):
+        stop = bool(n_clicks & 1)
+        return stop, f"{'Start' if stop else 'Stop'} Refresh"
+    
+    app.run(debug=True)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -162,11 +247,16 @@ def main():
                         help="Output file for toolhead coordinates")
     parser.add_argument("--data", type=argparse.FileType('r'),
                         default=None, help="Toolhead coordinates data")
+    parser.add_argument("--real-time", action="store_true",
+                        help="Create dynamic graph")
     args = parser.parse_args()
 
+    positions = []
+    timestamps = []
     if args.data is None:
         connection = connect(socket_path)
         if connection is None:
+            print("Could not connect to emulator socket.")
             return -1
 
         id = get_toolhead_id(connection)
@@ -174,24 +264,41 @@ def main():
         if id == 0:
             return -1
 
-        positions = []
+        heater_ids = get_heaters_id(connection)
+        print(f"Heaters: {", ".join([str(h) for h in heater_ids])}")
+    
+    if args.real_time:
+        run_dash_server(connection, id, *heater_ids)
+        return 0
+
+    if args.data is None:
+        heaters = {x: [] for x in heater_ids}
         while True:
             try:
-                positions.append(get_position(connection, id))
+                timestamps.append(time.time())
+                position, temps = get_data(connection, id, *heater_ids)
+                positions.append(position)
+                for h in heater_ids:
+                    heaters[h].append(temps[h])
                 time.sleep(0.02)
             except KeyboardInterrupt:
                 break
+            except BrokenPipeError:
+                print("Emulator socket closed remotely.", file=sys.stderr)
+                return -1
 
         if args.csv:
             c = csv.writer(args.csv)
-            for p in positions:
+            for i, p in enumerate(positions):
+                for h in heater_ids:
+                    p.append(heaters[h][i])
                 c.writerow(p)
             args.csv.close()
     else:
         c = csv.reader(args.data)
         positions = [x for x in c]
         args.data.close()
-    draw(args.graph, positions)
+    draw(args.graph, positions, timestamps, heaters)
     args.graph.close()
     return 0
 
