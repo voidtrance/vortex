c9c394ca8584a53a84bb75db5621bc72cbf31af3
Mitko Haralanov <voidtrance@gmail.com>
2025-03-19 09:17:20 -0700

    Add support for "sequential" printing
    Sequential printing is when the tool sends gcode commands to
    Klipper one-by-one instead of starting a print through the
    `virtual_sdcard` object. This ability is useful for debugging
    when we want to slow down the print and see at which point
    things go wrong.

--- tools/klipper_print_file.py
+++ tools/klipper_print_file.py
@@ -18,6 +18,7 @@ import sys
 import os
 import time
 import json
+import pickle
 import socket
 import argparse
 import configparser
@@ -27,7 +28,9 @@ seq = parser.add_argument_group("Sequential Printing",
                                 description="Print file by sending commands one by one.")
 seq.add_argument("--sequential", action="store_true", help="Enable sequential printing")
 seq.add_argument("--interval", type=float, default=0.0, help="Send commands every INTERVAL seconds")
-seq.add_argument("--wait", action="store_true", help="Wait for use intput before continuing")
+seq.add_argument("--wait", action="store_true", help="Wait for user intput before continuing")
+seq.add_argument("-M", action="store_true", dest="monitor", help="Monitor emulator")
+seq.add_argument("--emulator-pipe", default="/tmp/vortex_monitor", help="Emulator command pipe")
 parser.add_argument("--pipe", default="/tmp/printer", help="Klipper command pipe")
 parser.add_argument("config", help="Klipper config file")
 
@@ -60,27 +63,44 @@ except KeyboardInterrupt:
 
 print(f"Printing {gcode_file}...")
 if args.sequential:
+    if args.monitor:
+        emulator = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        try:
+            emulator.connect(args.emulator_pipe)
+        except (FileNotFoundError, ConnectionRefusedError):
+            print("Could not connect to emulator socket.")
+            sys.exit(-1)
+
     printer = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     printer.connect(args.pipe)
     id = 0
-    with open(os.path.join(gcode_path, gcode_file), 'r') as gcode:
-        for line in gcode:
-            if not line.strip() or line.startswith(';'):
-                continue
-            print(f"Executing {line.strip()}...")
-            data = json.dumps({'id': id, 'method': 'gcode/script',
-                               'params': {'script': line.strip()}}).encode()
-            printer.sendall(data + chr(0x3).encode())
-            id += 1
-            response = printer.recv(1024)
-            if args.wait:
-                input("Next line? ")
-            elif args.interval:
-                time.sleep(args.interval)
+    try:
+        with open(os.path.join(gcode_path, gcode_file), 'r') as gcode:
+            for line in gcode:
+                emulator.sendall(pickle.dumps({"request": "object_list"}))
+                data = emulator.recv(8192)
+                if not line.strip() or line.startswith(';'):
+                    continue
+                print(f"Executing {line.strip()}...")
+                data = json.dumps({'id': id, 'method': 'gcode/script',
+                                   'params': {'script': line.strip()}}).encode()
+                printer.sendall(data + chr(0x3).encode())
+                id += 1
+                response = printer.recv(1024)
+                if args.wait:
+                    input("Next line? ")
+                elif args.interval:
+                    time.sleep(args.interval)
+    except BrokenPipeError:
+        sys.exit(0)
+    finally:
+        printer.close()
+        if args.monitor:
+            emulator.close()
 else:
     printer = open(args.pipe, 'a')
     printer.write("M21\n")
     printer.write(f"M23 {gcode_file}\n")
     printer.write("M24\n")
 
-    
+sys.exit(0)
