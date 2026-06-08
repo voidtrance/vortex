voidtrance/vortex

commit 66bff8e626d2802da4a3f990f9bb7ee6a39a0237

Convert object_defs.py to auto-generated file
In order for the Python side of the emulator code to be able to
communicate with the HW objects, structures used the HW objects
for configuration, state, command arguments, etc. have to be
replicated in Python using the `ctypes` module.

However, it is possible for the Python structure definitions to
be auto-generated by parsing the C HW object code.

This is what this change does. It extendes the `tools/auto_gen.py`
tool to parse all of the HW object files and create a new
`object_defs.py` file containing the necessary structures.

This new method does require changes in development processes. Now,
HW object structures have to have specific name patterns, which have
been outlined in the documentation.

Makefile
@@ -1,5 +1,5 @@
 # vortex - GCode machine emulator
-# Copyright (C) 2024-2025 Mitko Haralanov
+# Copyright (C) 2024-2026 Mitko Haralanov
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -38,7 +38,7 @@ ifeq ($(DEBUG),1)
 endif
 
 all: version
-	$(GCC_BUILD_OPTS) $(PYTHON) -m pip install --no-build-isolation \
+	CC=clang $(GCC_BUILD_OPTS) $(PYTHON) -m pip install --no-build-isolation \
 		--editable . $(MESON_BUILD_OPTS)
 	@if [ ! -L compile_commands.json ]; then \
 		ln -s build/cp$(word 1,$(PYTHON_VERSION_NUMS))$(word 2,$(PYTHON_VERSION_NUMS))/compile_commands.json \
@@ -74,7 +74,7 @@ venv:
 	$(VENV)/bin/pip install -r ./virtualenv.txt
 
 wheel: venv version
-	$(VENV_PYTHON) -m build -w .
+	CC=clang $(VENV_PYTHON) -m build -w .
 
 package: version
 	$(PYTHON) -m build -w .

src/core/objects/axis.c
@@ -1,6 +1,6 @@
 /*
  * vortex - GCode machine emulator
- * Copyright (C) 2024-2025 Mitko Haralanov
+ * Copyright (C) 2024-2026 Mitko Haralanov
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -47,15 +47,8 @@ typedef struct {
 } axis_motor_t;
 
 typedef struct {
-    float length;
-    const char type;
-    const char **steppers;
-    const char endstop[ENDSTOP_NAME_SIZE];
-} axis_config_params_t;
-
-typedef struct {
     core_object_t object;
-    axis_type_t type;
+    kinematics_axis_type_t type;
     axis_motor_t *motors;
     size_t n_motors;
     const char *endstop_name;
@@ -74,9 +67,10 @@ typedef struct {
 typedef int (*command_func_t)(core_object_t *object, void *args);
 
 static object_cache_t *axis_event_cache = NULL;
-static coordinates_t randomized_motor_position = { 0 };
+static kinematics_coordinates_t randomized_motor_position = { 0 };
 
-static void axis_event_handler(core_object_t *object, const char *name,
+static void axis_event_handler(core_object_t *object,
+                               const char *name,
                                const core_object_event_type_t event,
                                void *data);
 
@@ -110,14 +104,13 @@ static void axis_reset(core_object_t *object) {
                 spmm = axis->motors[1].steps_per_mm;
             break;
         case KINEMATICS_DELTA:
-            if (randomized_motor_position.a == 0.0 &&
-                randomized_motor_position.b == 0.0 &&
+            if (randomized_motor_position.a == 0.0 && randomized_motor_position.b == 0.0 &&
                 randomized_motor_position.c == 0.0) {
                 delta_kinematics_config_t *delta_config =
                     (delta_kinematics_config_t *)kinematics_get_config();
                 float max_x = delta_config->radius * sin(DEG2RAD(45));
                 float max_y = delta_config->radius * cos(DEG2RAD(45));
-                coordinates_t position = { 0 };
+                kinematics_coordinates_t position = { 0 };
 
                 log_debug(axis, "Axis X min/max: %f/%f", -max_x, max_x);
                 log_debug(axis, "Axis Y min/max: %f/%f", -max_y, max_y);
@@ -128,8 +121,7 @@ static void axis_reset(core_object_t *object) {
                 position.z = random_float_limit(0.0, delta_config->z_length);
                 log_debug(axis, "Position Z: %f", position.z);
 
-                kinematics_get_motor_movement(&position,
-                                              &randomized_motor_position);
+                kinematics_get_motor_movement(&position, &randomized_motor_position);
             }
 
             switch (axis->type) {
@@ -166,16 +158,13 @@ static int axis_init(core_object_t *object) {
     size_t i;
 
     for (i = 0; i < axis->n_motors; i++) {
-        axis->motors[i].obj = CORE_LOOKUP_OBJECT(axis, OBJECT_KLASS_STEPPER,
-                                                 axis->motors[i].name);
+        axis->motors[i].obj = CORE_LOOKUP_OBJECT(axis, OBJECT_KLASS_STEPPER, axis->motors[i].name);
         if (!axis->motors[i].obj) {
-            log_error(axis, "Failed to find stepper motor %s",
-                      axis->motors[i].name);
+            log_error(axis, "Failed to find stepper motor %s", axis->motors[i].name);
             return -ENODEV;
         }
 
-        axis->motors[i].obj->get_state(axis->motors[i].obj,
-                                       &axis->stepper_status);
+        axis->motors[i].obj->get_state(axis->motors[i].obj, &axis->stepper_status);
         axis->motors[i].move_complete = true;
         axis->motors[i].steps_per_mm = axis->stepper_status.steps_per_mm;
         axis->motors[i].initial_step = axis->stepper_status.steps;
@@ -184,8 +173,7 @@ static int axis_init(core_object_t *object) {
 
     if (axis->endstop_name) {
         endstop_status_t status;
-        axis->endstop =
-            CORE_LOOKUP_OBJECT(axis, OBJECT_KLASS_ENDSTOP, axis->endstop_name);
+        axis->endstop = CORE_LOOKUP_OBJECT(axis, OBJECT_KLASS_ENDSTOP, axis->endstop_name);
         if (!axis->endstop) {
             log_error(axis, "Failed to find endstop %s", axis->endstop_name);
             return -ENODEV;
@@ -199,29 +187,27 @@ static int axis_init(core_object_t *object) {
     }
 
     axis->endstop_event_token = CORE_EVENT_REGISTER(axis, OBJECT_KLASS_ENDSTOP,
-                                                    OBJECT_EVENT_ENDSTOP_TRIGGER,
+                                                    OBJECT_EVENT_ENDSTOP_TRIGGERED,
                                                     axis->endstop_name, axis_event_handler);
     axis_reset(object);
     return 0;
 }
 
-static void axis_event_handler(core_object_t *object, const char *name,
+static void axis_event_handler(core_object_t *object,
+                               const char *name,
                                const core_object_event_type_t event,
                                void *data) {
-    endstop_trigger_event_data_t *event_data =
-        (endstop_trigger_event_data_t *)data;
+    endstop_triggered_event_data_t *event_data = (endstop_triggered_event_data_t *)data;
     axis_t *axis = (axis_t *)object;
 
-    if (!axis->homed && event == OBJECT_EVENT_ENDSTOP_TRIGGER &&
-        event_data->triggered)
+    if (!axis->homed && event == OBJECT_EVENT_ENDSTOP_TRIGGERED && event_data->triggered)
         axis->homed = true;
 }
 
-#define step_distance(motor)                                          \
-    ((double)(motor.steps - motor.initial_step) / motor.steps_per_mm)
+#define step_distance(motor) ((double)(motor.steps - motor.initial_step) / motor.steps_per_mm)
 
-static inline void set_axis_distance(axis_t *axis, coordinates_t *coords,
-                                     double distance) {
+static inline void
+set_axis_distance(axis_t *axis, kinematics_coordinates_t *coords, double distance) {
     switch (axis->type) {
     case AXIS_TYPE_X:
         coords->x = distance;
@@ -249,7 +235,7 @@ static inline void set_axis_distance(axis_t *axis, coordinates_t *coords,
     }
 }
 
-static inline double get_axis_distance(axis_t *axis, coordinates_t *coords) {
+static inline double get_axis_distance(axis_t *axis, kinematics_coordinates_t *coords) {
     double distance;
 
     switch (axis->type) {
@@ -281,11 +267,10 @@ static inline double get_axis_distance(axis_t *axis, coordinates_t *coords) {
     return axis->start_position + distance;
 }
 
-static void axis_update(core_object_t *object, uint64_t ticks,
-                        uint64_t runtime) {
+static void axis_update(core_object_t *object, uint64_t ticks, uint64_t runtime) {
     axis_t *axis = (axis_t *)object;
-    coordinates_t coords = { 0 };
-    coordinates_t distance = { 0 };
+    kinematics_coordinates_t coords = { 0 };
+    kinematics_coordinates_t distance = { 0 };
     kinematics_type_t kinematics = kinematics_type_get();
     double average_distance = 0.0;
     size_t i;
@@ -297,8 +282,7 @@ static void axis_update(core_object_t *object, uint64_t ticks,
      *      steps the stepper can perform.
      */
     for (i = 0; i < axis->n_motors; i++) {
-        axis->motors[i].obj->get_state(axis->motors[i].obj,
-                                       &axis->stepper_status);
+        axis->motors[i].obj->get_state(axis->motors[i].obj, &axis->stepper_status);
         axis->motors[i].steps = axis->stepper_status.steps;
     }
 
@@ -365,8 +349,7 @@ axis_t *object_create(const char *name, void *config_ptr) {
     kinematics_type_t kinematics = kinematics_type_get();
     size_t i = 0;
 
-    if (object_cache_create(&axis_event_cache,
-                            sizeof(axis_homed_event_data_t)))
+    if (object_cache_create(&axis_event_cache, sizeof(axis_homed_event_data_t)))
         return NULL;
 
     axis = calloc(1, sizeof(*axis));
@@ -390,9 +373,8 @@ axis_t *object_create(const char *name, void *config_ptr) {
      * defined.
      */
     stepper = *config->steppers;
-    while (stepper) {
+    while (stepper)
         stepper = config->steppers[++i];
-    }
 
     axis->motors = calloc(i, sizeof(*axis->motors));
     if (!axis->motors) {
@@ -408,13 +390,11 @@ axis_t *object_create(const char *name, void *config_ptr) {
 
     switch (kinematics) {
     case KINEMATICS_COREXY:
-        if ((axis->type == AXIS_TYPE_X || axis->type == AXIS_TYPE_Y) &&
-            axis->n_motors != 2)
+        if ((axis->type == AXIS_TYPE_X || axis->type == AXIS_TYPE_Y) && axis->n_motors != 2)
             goto error;
     case KINEMATICS_COREXZ:
         if (kinematics == KINEMATICS_COREXZ &&
-            (axis->type == AXIS_TYPE_X || axis->type == AXIS_TYPE_Z) &&
-            axis->n_motors != 2)
+            (axis->type == AXIS_TYPE_X || axis->type == AXIS_TYPE_Z) && axis->n_motors != 2)
             goto error;
     case KINEMATICS_CARTESIAN: {
         cartesian_kinematics_config_t *kin_config __maybe_unused;

src/threads/core_threads.c
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #define _GNU_SOURCE
-#include <stdio.h>
 #include <errno.h>
 #include <pthread.h>
 #include <stdlib.h>

tools/clang_ast/clang_ast_builder.py
@@ -1,0 +1,116 @@
+# vortex - GCode machine emulator
+# Copyright (C) 2024,2026 Mitko Haralanov
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from cffi import FFI
+import argparse
+import os
+
+def get_clang_cursor_kind_defs():
+    filename = "/usr/include/clang-c/Index.h"
+    enum_found = False
+    defs = ["enum CursorKind {"]
+    enum_values = {}
+    with open(filename, 'r') as fd:
+        for line in fd.readlines():
+            line = line.strip()
+            if enum_found:
+                if line == "};":
+                    break
+                if not line.startswith("CXCursor_"):
+                    continue
+                name, value = line.split(" = ")
+                try:
+                    value, _ = value.split(",")
+                except ValueError:
+                    if value[-1] == ',':
+                        value = value[:-1]
+                if value.isnumeric():
+                    enum_values[name] = value
+                else:
+                    value = enum_values[value]
+                _, py_name = name.split("_")
+                defs.append(f"    {py_name} = {value},")
+            else:
+                if line.startswith("enum CXCursorKind {"):
+                    enum_found = True
+    defs +=  [f"    Any = {str(int(value) + 1)},"]
+    defs += ["};"]
+    return defs
+
+def get_clang_type_kind_defs():
+    filename = "/usr/include/clang-c/Index.h"
+    enum_found = False
+    defs = ["enum TypeKind {"]
+    enum_values = {}
+    with open(filename, 'r') as fd:
+        for line in fd.readlines():
+            line = line.strip()
+            if enum_found:
+                if line == "};":
+                    break
+                if not line.startswith("CXType_"):
+                    continue
+                name, value = line.split(" = ")
+                try:
+                    value, _ = value.split(",")
+                except ValueError:
+                    if value[-1] == ',':
+                        value = value[:-1]
+                if value.isnumeric():
+                    enum_values[name] = value
+                else:
+                    value = enum_values[value]
+                _, py_name = name.split("_", 1)
+                defs.append(f"    {py_name} = {value},")
+            else:
+                if line.startswith("enum CXTypeKind {"):
+                    enum_found = True
+    for name in ("Char", "Size", "SSize"):
+        defs += [f" {name} = {str(int(value) + 1)},"]
+    defs += ["};"]
+    return defs
+
+def make_builder(header_path, link_path):
+    ffibuilder = FFI()
+    enum_cdef = get_clang_cursor_kind_defs()
+    enum_cdef += get_clang_type_kind_defs()
+    with open(os.path.join(header_path, "clang_ast.h"), "r") as fd:
+        cdef = []
+        for line in fd.readlines():
+            if line.startswith("#"):
+                continue
+            cdef.append(line.rstrip())
+        ffibuilder.cdef("\n".join(enum_cdef + cdef))
+
+    builder = ["#include \"clang_ast.h\""] + enum_cdef
+    ffibuilder.set_source("py_clang_ast", "\n".join(builder),
+                          libraries=["clang_ast"],
+                          include_dirs=[header_path],
+                          library_dirs=[link_path],
+                          extra_link_args=["-Wl,-rpath=$ORIGIN/tools/clang_ast"])
+    return ffibuilder
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-I', required=True, dest="header_path")
+    parser.add_argument("-L", required=True, dest="link_path")
+    opts = parser.parse_args()
+
+    curdir = os.getcwd()
+    header_path = os.path.abspath(os.path.join(curdir, opts.header_path))
+    link_path = os.path.abspath(os.path.join(curdir, opts.link_path))
+    ffibuilder = make_builder(header_path, link_path)
+    ret = ffibuilder.compile(verbose=3)
+    print(ret)

tools/find_modules.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # vortex - GCode machine emulator
-# Copyright (C) 2024-2025 Mitko Haralanov
+# Copyright (C) 2024-2026 Mitko Haralanov
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,19 +14,23 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-import re
 import os
+import pathlib
+from clang import cindex
 import argparse
 
-def find_hw_objects(source_path):
-    object_reg = re.compile(r'^class (?P<klass>[^\(]+)\(ObjectDef\):$', re.MULTILINE)
+def find_hw_objects(source_dir):
+    object_dir = pathlib.PosixPath(source_dir)
+    index = cindex.Index.create()
     objects = []
-    with open(os.path.join(source_path, "controllers/objects/object_defs.py")) as fd:
-        for line in fd:
-            match = object_reg.match(line)
-            if not match:
-                continue
-            objects.append(match.group("klass").strip().lower())
+    for file in object_dir.iterdir():
+        if file.suffix != ".c":
+            continue
+        tu = index.parse(file)
+        for node in tu.cursor.walk_preorder():
+            if node.kind == cindex.CursorKind.FUNCTION_DECL and \
+                node.spelling == "object_create":
+                objects.append(file.stem)
     return objects
 
 def main():

controllers/__init__.py
@@ -1,5 +1,5 @@
 # vortex - GCode machine emulator
-# Copyright (C) 2024-2025 Mitko Haralanov
+# Copyright (C) 2024-2026 Mitko Haralanov
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -246,7 +246,7 @@ class Controller(core.VortexCore):
         return vobjs
 
     def _load_objects(self, config):
-        module = importlib.import_module("vortex.controllers.objects.object_defs")
+        module = importlib.import_module("vortex.core.objects.structs")
         base = getattr(module, "ObjectDef")
         members = inspect.getmembers(module, inspect.isclass)
         members = [x for x in members if x[1] is not base]
@@ -292,7 +292,7 @@ class Controller(core.VortexCore):
                 self.object_defs[klass] = self.object_factory[klass]
                 obj_conf = self.object_factory[klass].config()
                 if klass == core.ObjectKlass.THERMISTOR:
-                    options.adc_max = self.ADC_MAX
+                    options.max_adc = self.ADC_MAX
                 if klass == core.ObjectKlass.PWM:
                     options.pwm_max = self.PWM_MAX
                 options = vortex.lib.ctypes_helpers.expand_substruct_config(options,

controllers/objects/meson.build
@@ -1,5 +1,5 @@
 # vortex - GCode machine emulator
-# Copyright (C) 2024-2025 Mitko Haralanov
+# Copyright (C) 2024-2026 Mitko Haralanov
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,5 +13,4 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-py.install_sources(['object_defs.py'], subdir: 'vortex/controllers/objects')
 subdir('virtual')

commit 5a1b883b14c4084ac0ef94428293101cb5c62d64

Fix GitHub workflows
Switching to Clang with commit 66bff8e ("Convert object_defs.py
to auto-generated file") caused the GitHub build/release workflows
to stop working due to the new requirements for Clang, LLVM, and
the Clang Python bindings.

Fixing this requires adding new GitHub actions to install LLVM and
Clang, configure the build environment and make some code changes
to allow for emulator to compile cleanly.

.github/workflows/build-arm.yml
@@ -44,6 +44,7 @@ jobs:
         uses: actions/setup-python@v5
         with:
           python-version: '3.13'
+          cache: 'pip'
       - name: Get Version
         id: vortex-version
         run: echo "vortex-version=$(git describe --tags --abbrev=0)" >> $GITHUB_OUTPUT
@@ -56,12 +57,21 @@ jobs:
           sudo apt install -y meson libgirepository-2.0-dev libcairo2-dev
           python -m pip install --upgrade pip setuptools wheel flake8 meson
           pip install -r virtualenv.txt
-#      - name: Lint with flake8
-#        run: |
-#          # stop the build if there are Python syntax errors or undefined names
-#          flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
-#          # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
-#          flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
+      - name: Install LLVM and Clang
+        uses: KyleMayes/install-llvm-action@v2
+        with:
+          version: "21.1.8"
+          env: 1
+      - name: Install Python Dependencies
+        run: |
+          python -m pip install --upgrade pip
+          python -m pip install clang clang-format
+      - name: Setup Environment
+        run: |
+          echo CLANG_INCLUDE_PATH=$(llvm-config --includedir) >> $GITHUB_ENV
+          echo LIBRARY_PATH=$(llvm-config --libdir) >> $GITHUB_ENV
+          echo LLVM_CFLAGS=$(llvm-config --cflags) >> $GITHUB_ENV
+          echo CC=clang >> $GITHUB_ENV
       - name: Build emulator ${{ steps.vortex-version.outputs.vortex-version }}
         run: make package
       - name: Upload asset

.github/workflows/build-x64.yml
@@ -44,6 +44,7 @@ jobs:
         uses: actions/setup-python@v5
         with:
           python-version: '3.13'
+          cache: 'pip'
       - name: Get Version
         id: vortex-version
         run: echo "vortex-version=$(git describe --tags --abbrev=0)" >> $GITHUB_OUTPUT
@@ -56,12 +57,21 @@ jobs:
           sudo apt install -y meson libgirepository-2.0-dev libcairo2-dev
           python -m pip install --upgrade pip setuptools wheel flake8 meson
           pip install -r virtualenv.txt
-#      - name: Lint with flake8
-#        run: |
-#          # stop the build if there are Python syntax errors or undefined names
-#          flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
-#          # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
-#          flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
+      - name: Install LLVM and Clang
+        uses: KyleMayes/install-llvm-action@v2
+        with:
+          version: "21.1.8"
+          env: 1
+      - name: Install Python Dependencies
+        run: |
+          python -m pip install --upgrade pip
+          python -m pip install clang clang-format
+      - name: Setup Environment
+        run: |
+          echo CLANG_INCLUDE_PATH=$(llvm-config --includedir) >> $GITHUB_ENV
+          echo LIBRARY_PATH=$(llvm-config --libdir) >> $GITHUB_ENV
+          echo LLVM_CFLAGS=$(llvm-config --cflags) >> $GITHUB_ENV
+          echo CC=clang >> $GITHUB_ENV
       - name: Build emulator ${{ steps.vortex-version.outputs.vortex-version }}
         run: make package
       - name: Upload asset

Makefile
@@ -24,7 +24,6 @@ VENV ?=
 VENV_PYTHON := $(VENV)/bin/python3
 DEBUG_OPTS :=
 MESON_DEBUG_OPTS :=
-GCC_BUILD_OPTS :=
 VERSION=$(shell git describe --tags --abbrev=0)
 KVER := $(shell uname -r)
 ARCH := $(shell uname -m)
@@ -32,17 +31,33 @@ ARCH := $(shell uname -m)
 PYTHON_VERSION=$(shell $(PYTHON) -c "import platform; print(platform.python_version())")
 PYTHON_VERSION_NUMS = $(subst ., ,$(PYTHON_VERSION))
 
+# Setup Clang
+CLANG_INCLUDE_DIR=$(shell llvm-config --includedir)
+CLANG_LIB_DIR=$(shell llvm-config --libdir)
+LIBRARY_PATH=$(shell llvm-config --libdir)
+CC=clang
+LD_LIBRATY_PATH += $(shell llvm-config --libdir)
+CFLAGS += $(shell llvm-config --cflags)
+LDFLAGS += -L$(shell llvm-config --libdir)
+
 ifeq ($(DEBUG),1)
-	GCC_BUILD_OPTS=CFLAGS='-DVORTEX_DEBUG -g
+	CFLAGS += -DVORTEX_DEBUG -g
 	ifeq ($(TIMER_DEBUG),1)
-		GCC_BUILD_TOPS += -DVORTEX_TIMERS_DEBUG
+		CFLAGS += -DVORTEX_TIMERS_DEBUG
 	endif
-	GCC_BUILD_OPTS += '
 	MESON_BUILD_OPTS=--config-settings=setup-args="-Dbuildtype=debug"
 endif
 
+export CC
+export CFLAGS
+export LDFLAGS
+export CLANG_INCLUDE_DIR
+export CLANG_LIB_DIR
+export LIBRARY_PATH
+export LD_LIBRARY_PATH
+
 all: version
-	CC=clang $(GCC_BUILD_OPTS) $(PYTHON) -m pip install --no-build-isolation \
+	$(PYTHON) -m pip install --no-build-isolation \
 		--editable . $(MESON_BUILD_OPTS)
 	@if [ ! -L compile_commands.json ]; then \
 		ln -s build/cp$(word 1,$(PYTHON_VERSION_NUMS))$(word 2,$(PYTHON_VERSION_NUMS))/compile_commands.json \
@@ -78,7 +93,7 @@ venv:
 	$(VENV)/bin/pip install -r ./virtualenv.txt
 
 wheel: venv version
-	CC=clang $(VENV_PYTHON) -m build -w .
+	$(VENV_PYTHON) -m build -w .
 
 package: version
 	$(PYTHON) -m build -w .

src/core/mem_debug.c
@@ -15,7 +15,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#define _GNU_SOURCE
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE 1
+#endif
 #include <stdio.h>
 #include <dlfcn.h>
 #include <stdbool.h>

src/core/objects/axis.c
@@ -15,7 +15,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#define _GNU_SOURCE
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE 1
+#endif
 #include <stdint.h>
 #include <string.h>
 #include <stdbool.h>

src/lib/logging/logging.c
@@ -15,7 +15,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#define _GNU_SOURCE
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE 1
+#endif
 #include <stdio.h>
 #include <stddef.h>
 #include <stdint.h>

src/lib/random.c
@@ -15,7 +15,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#define _GNU_SOURCE
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE 1
+#endif
 #include <pthread.h>
 #include <stdlib.h>
 #include <values.h>

src/threads/core_threads.c
@@ -15,7 +15,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#define _GNU_SOURCE
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE 1
+#endif
 #include <errno.h>
 #include <pthread.h>
 #include <stdlib.h>

tools/clang_ast/clang_ast_builder.py
@@ -18,7 +18,8 @@ import argparse
 import os
 
 def get_clang_cursor_kind_defs():
-    filename = "/usr/include/clang-c/Index.h"
+    include_path = os.environ.get("CLANG_INCLUDE_PATH", "/usr/include")
+    filename = os.path.join(include_path, "clang-c/Index.h")
     enum_found = False
     defs = ["enum CursorKind {"]
     enum_values = {}
@@ -50,7 +51,8 @@ def get_clang_cursor_kind_defs():
     return defs
 
 def get_clang_type_kind_defs():
-    filename = "/usr/include/clang-c/Index.h"
+    include_path = os.environ.get("CLANG_INCLUDE_PATH", "/usr/include")
+    filename = os.path.join(include_path, "clang-c/Index.h")
     enum_found = False
     defs = ["enum TypeKind {"]
     enum_values = {}

tools/find_modules.py
@@ -21,6 +21,9 @@ import argparse
 
 def find_hw_objects(source_dir):
     object_dir = pathlib.PosixPath(source_dir)
+    libdir = os.environ.get("CLANG_LIB_DIR")
+    if libdir:
+        cindex.Config.set_library_path(libdir)
     index = cindex.Index.create()
     objects = []
     for file in object_dir.iterdir():